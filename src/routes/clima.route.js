ad7802f595dd2879e694a3bf27ec6ac1d5bd603f
Mar Calmon <marical2222@gmail.com>
2019-02-20 09:54:42 -0300

    Agregue rutas.

--- src/routes/clima.route.js
+++ src/routes/clima.route.js
@@ -1,8 +1,18 @@
 const express = require ('express')
+const path = require('path')
 const router = express.Router()
+const axios = require ('axios')
 
-router.get('/', (req, res) => {
+router.get('/v1', (req, res) => {
   res.send('Index page');
+})
+
+router.get('/location', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'public', 'listado.html'))
+})
+
+router.get('/current[/city]', function (req, res) {
+  res.send(path.join(__dirname, '..', 'public', 'detalle.html'))
 })
 
 module.exports = router
