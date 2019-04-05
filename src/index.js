d05a18b00ead4968bd3f3bc85f8d808eb089ca9f
Mar Calmon <marical2222@gmail.com>
2019-04-05 09:04:06 -0300

    Configuración de CORS.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ const morgan = require ('morgan')
 const app = express()
 
 //Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 1234)
 
 //Middlewares
 app.use(morgan('dev'))
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, FETCH');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Routes
 app.use(require('./routes/clima.route'))
